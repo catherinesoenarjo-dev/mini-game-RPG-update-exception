b2a7b5bec2e9e57ec55f5c9803472963b9ab15e1
catherinesoenarjo-dev <catherinesoenarjo@gmail.com>
2026-05-18 11:18:00 +0800

    Add files via upload

---
+++ mini game RPG update/musuh.java
@@ -0,0 +1,18 @@
+public abstract class musuh {
+
+    protected String namaMusuh;
+    protected int healthPoint;
+
+    public musuh(String nama, int hp) {
+        this.namaMusuh = nama;
+        this.healthPoint = hp;
+    }
+
+    public void terimaDamage(int damage) {
+        this.healthPoint -= damage;
+        System.out.println(this.namaMusuh + " terkena serangan biasa. Sisa HP: " + this.healthPoint);
+    }
+
+    public abstract void serangPemain();
+    public abstract void suaraKhas();
+}
